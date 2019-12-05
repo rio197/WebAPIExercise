132c0dce31201c9f88f9d93a379daac5b363c2ef
Thomas M <thomas@test.com>
2019-12-04 22:21:58 -0500

    Initial Commit

---
+++ WebApplication4/Controllers/ContactController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ContactController : ApiController
+    {
+        Contact[] contacts = new Contact[]
+        {
+            new Contact() { Id = 0,FirstName = "Peter", LastName = "Parker" },
+            new Contact() { Id = 1,FirstName = "Bruce", LastName = "Wayne" },
+            new Contact() { Id = 2,FirstName = "Bruce", LastName = "Banner" }
+        };
+
+        // GET: api/Contact
+        public IEnumerable<Contact> Get()
+        {
+            return contacts;
+        }
+
+        // GET: api/Contact/5
+        public IHttpActionResult Get(int id)
+        {
+            Contact contact = contacts.FirstOrDefault<Contact>(c => c.Id == id);
+
+            if(contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        // POST: api/Contact
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Contact/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Contact/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
